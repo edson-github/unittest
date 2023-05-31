6d6b9312d8acfe88f06c23ff7c03149447d95cfb
Sourcery AI <>
2023-05-31 23:13:33 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Product:
 
     def increase_stock(self, stock_to_add: int):
         self.check_positive_number(stock_to_add)
-        self.stock: int = self.stock + stock_to_add
+        self.stock += stock_to_add
     
     def decrease_stock(self, stock_to_reduce):
         self.check_positive_number(stock_to_reduce: int)
